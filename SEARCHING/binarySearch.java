376b0e3cd63284cf6889f798be968c93654444a8
Aman Shaw <amanshaw817@gmail.com>
2025-09-14 21:21:09 +0530

    updated

--- SEARCHING/binarySearch.java
+++ SEARCHING/binarySearch.java
@@ -19,11 +19,30 @@ public class binarySearch {
     return false;
     }
 
+    static boolean recursiveBinarySearch(int[] a,int start,int end,  int target){
+            if(start>end){
+                return false;
+            }
+            int mid = (start + end)/2;
+            if(target == a[mid]){
+                return true;
+            }
+            else if(target<a[mid]){
+                return recursiveBinarySearch(a, start,  mid-1, target);
+            }
+            else if (target>a[mid]){
+                return recursiveBinarySearch(a, mid+1, end, target);
+            }
+        return false;
+    }
+
 public static void main(String[] args) {
     int[] a = {1, 2, 3, 4, 5};
     int target = 4;
 
     boolean result = binarySearch(a, target);
     System.out.println(result);
+    boolean recursiveResult = recursiveBinarySearch(a, 0, a.length-1, target);
+    System.out.println(recursiveResult);
     }
 }
